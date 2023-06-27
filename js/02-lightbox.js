8a6340ca6ad7cf5aae7910d577e67b34127aa91c
Cassandder <nocte.vindicem@gmail.com>
2023-06-28 00:20:51 +0200

    add-gallery02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', function() {
+  const galleryContainer = document.querySelector('.gallery');
+  galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+  function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) =>
+      `<li class="gallery__item">
+         <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+         </a>
+       </li>`
+    ).join('');
+  }
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+  });
+});
+
+
+
+
+
+
+
+ 
